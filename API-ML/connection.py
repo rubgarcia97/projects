83ecbeff8d80d1bda3a7896fd6b2fcf5a8aaf80e
rubgarcia97 <rubengarciaolivas@gmail.com>
2023-05-07 19:18:14 +0200

    update

--- API-ML/connection.py
+++ API-ML/connection.py
@@ -1,29 +1,61 @@
-import requests
+import json
+import os
+import math
 import pandas as pd
+import requests
 
 from environment import Environment
 
 environment = Environment()
 
-#Creamos carpeta donde se alojaran nuestras peticiones
-environment.create_folder("files")
+content_dir = 'src/files'
+file_name = 'DisasterDeclarationsSummaries'
+full_path = os.path.join(content_dir, file_name)
 
-#Configuración básica de la API
-base_url="https://www.fema.gov/api/open/v2/"
-endpoint="DisasterDeclarationsSummaries"
-params={
-    '$top':10
+# Configuración básica de la API, al estar esta limitada a 1000 resultado por petición,
+# hacemos una petición para conocer el numero de resultados totales que se disponen
+base_url = "https://www.fema.gov/api/open/v2/"
+endpoint = "DisasterDeclarationsSummaries"
+params = {
+    "$orderby": "declarationDate desc",
+    "$inlinecount": "allpages",
+    "$select": "id",
+    "$top": 1,
 }
 
-url=base_url+endpoint
+url = base_url + endpoint
 
-
-#Requests
 response = requests.get(url, params=params)
-json_content = response.json()["DisasterDeclarationsSummaries"]
-environment.create_file("Prueba",json_content)
+json_content = response.json()["metadata"]["count"]
+
+n_loop = math.ceil(json_content / 1000)
+
+# Generamos mediante un bucle tantas peticiones como son necesarias para traer toda la información
+
+environment.create_folder(content_dir)
 
 
-df = pd.read_json('./files/Prueba')
+skip = 0
+top = 1000
 
-df.to_csv('archivo.csv', index=False)
+outFile = open(full_path+".json", "a")
+# outFile.write('{"femawebdisasterdeclarations":[')
+
+i = 0
+while (i < n_loop):
+    response = requests.get(url, params="&$metadata=off&$format=jsona&$skip=" + str(skip) + "&$top=" + str(top))
+    json_content = response.json()
+
+    if i == (n_loop - 1):
+        outFile.write(json.dumps(json_content[1:-1]) + "]")
+    else:
+        outFile.write(json.dumps(json_content[1:-1]) + ",")
+
+    i += 1
+    skip = i * 1000
+
+outFile.close()
+
+
+df = pd.read_json(full_path+'.json')
+df.to_csv(content_dir+'/'+file_name+'.csv', index=False)
